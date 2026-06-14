a424e8c524160cd7ad4482feb14e5d27594b5c8c
Arvin Garcia <arvin@arvingarcia.com>
2026-06-14 09:30:27 +0800

    fix: remove static headings in sidebar

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,17 +2,24 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  // load .md files in the `src/content/blog/` directory
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
 
-  // type-check frontmatter using a schema
   schema: ({ image }) =>
     z.object({
       title: z.string(),
       description: z.string(),
-      pubDate: z.coerce.date(), // transform string to Date object
+      pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
       heroImage: image().optional(),
+      headings: z
+        .array(
+          z.object({
+            depth: z.number().min(2).max(3),
+            slug: z.string(),
+            text: z.string(),
+          })
+        )
+        .optional(),
     }),
 });
 
